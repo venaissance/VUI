d6ecae0d375cede172d64211385501474cdb5898
VarianM <mizeyuyu@gmail.com>
2020-04-12 13:27:34 +0800

    添加 Icon 组件的 onClick 事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,12 +4,13 @@ import './icon.scss';
 
 interface IconProps {
   name: string;
+  onClick?: () => void;
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   return (
     <span>
-      <svg className="vui-icon">
+      <svg className="vui-icon" onClick={props.onClick}>
         <use xlinkHref={`#${props.name}`}/>
       </svg>
       <span>{props.name}</span>
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
+const fn = () => {
+  console.log('fn');
+};
+
 ReactDOM.render(<div>
-  <Icon name="wechat"/>
+  <Icon name="wechat" onClick={fn}/>
   <Icon name="alipay"/>
   <Icon name="user"/>
 </div>, document.querySelector('#root'));
