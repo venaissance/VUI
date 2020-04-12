5c7f134d4c5fa226f2dacac9bc3688d86d80e4f0
VarianM <mizeyuyu@gmail.com>
2020-04-12 13:23:27 +0800

    Icon 组件添加默认 CSS

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './importIcons.js';
+import './icon.scss';
 
 interface IconProps {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
+const Icon: React.FunctionComponent<IconProps> = props => {
   return (
     <span>
-      <svg>
+      <svg className="vui-icon">
         <use xlinkHref={`#${props.name}`}/>
       </svg>
-      {props.name}
+      <span>{props.name}</span>
     </span>
   );
 };
 
 export default Icon;
-
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import * as React from 'react';
 import Icon from './icon';
 
-
 ReactDOM.render(<div>
-    <Icon name="wechat"/>
-    <Icon name="alipay"/>
-    <Icon name="user"/>
-  </div>
-  , document.querySelector('#root'));
+  <Icon name="wechat"/>
+  <Icon name="alipay"/>
+  <Icon name="user"/>
+</div>, document.querySelector('#root'));
+
+
+
 
