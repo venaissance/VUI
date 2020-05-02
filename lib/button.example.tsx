74cae65745819a9491512ad90b34361cff502161
varian <mizeyuyu@gmail.com>
2020-05-02 20:11:01 +0800

    添加展示页 example.tsx

---
+++ lib/button.example.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ButtonExample: React.FunctionComponent = () => {
+  return (
+    <div>
+      A Button
+    </div>
+  );
+};
+
+export default ButtonExample;
---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,14 @@
+import Icon from './icon';
+import React from 'react';
+
+const IconExample: React.FunctionComponent = () => {
+  return (
+    <div>
+      <Icon name="wechat"/>
+      <Icon name="alipay"/>
+      <Icon name="user"/>
+    </div>
+  );
+};
+
+export default IconExample;
