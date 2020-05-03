f32a577b46bb79b618d04939878fa4d44a4b85d3
varian <mizeyuyu@gmail.com>
2020-05-03 20:18:15 +0800

    Dialog 组件接受自定义按钮（Array<ReactElement> + React.cloneElement，并添加关闭窗口功能

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -7,7 +7,10 @@ const DialogExample: React.FunctionComponent = () => {
     <div>
       <h1>Example 1</h1>
       <button onClick={() => setX(!x)}>click</button>
-      <Dialog visible={x}>
+      <Dialog visible={x} buttons={[
+        <button onClick={() => setX(false)}>1</button>,
+        <button onClick={() => setX(false)}>2</button>
+      ]} onClose={() => setX(false)}>
         <strong>dialog</strong>
       </Dialog>
     </div>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,23 +1,29 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, ReactElement} from 'react';
 import {Icon} from '../index';
 import {scopedClassMaker} from '../classes';
 import './dialog.scss';
 
 interface Props {
-  visible: boolean
+  visible: boolean,
+  buttons: Array<ReactElement>,
+  onClose: React.MouseEventHandler,
 }
 
 const scopedClass = scopedClassMaker('vui-dialog');
 const sc = scopedClass;
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
+  const onClickClose: React.MouseEventHandler = (e) => {
+    props.onClose(e);
+  };
+
   return (
     props.visible ?
       <Fragment>
         <div className={sc('mask')}>
         </div>
         <div className={sc()}>
-          <div className={sc('close')}>
+          <div className={sc('close')} onClick={onClickClose}>
             <Icon name="close"/>
           </div>
           <header className={sc('header')}>
@@ -27,6 +33,10 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
             {props.children}
           </main>
           <footer className={sc('footer')}>
+            {props.buttons &&
+            props.buttons.map((button, index) => {
+              React.cloneElement(button, {key: index});
+            })}
           </footer>
         </div>
       </Fragment>
