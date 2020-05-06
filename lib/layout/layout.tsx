dbcb828aab42d63ef56fe5a9d85dd61a73b17efc
VarianM <mizeyuyu@gmail.com>
2020-05-06 21:47:56 +0800

    函数式实现 Layout 组件检测里面有没有 Sider 子组件功能

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import {scopedClassMaker} from '../classes';
 import './layout.scss';
+import Sider from './sider';
 
 
 const sc = scopedClassMaker('vui-layout');
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
-
+  children: ReactElement | Array<ReactElement>
 }
 
 const Layout: React.FunctionComponent<Props> = (props) => {
-
   const {className, ...rest} = props;
-  console.log(props.children);
+  const children = props.children as Array<ReactElement>;
+  const hasSider = 'length' in children &&
+    children.reduce((result, node) => result || node.type === Sider, false);
 
   return (
-    <section className={sc('', {extra: className})} {...rest}>
+    <section className={sc('', {extra: [className, hasSider && 'hasSider'].join(' ')})} {...rest}>
       {props.children}
     </section>
   );
