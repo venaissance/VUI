2d0e6934961b65e3e539a04583db98078fd6ebde
varian <mizeyuyu@gmail.com>
2020-07-22 12:45:20 +0800

    优化生成官网脚本

--- doc.sh
+++ doc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-yarn doc
-git checkout gh-pages
-mv -f doc/* ./
-git add .
-git commit -m "update"
-git push
+yarn doc &&
+git checkout gh-pages &&
+mv -f doc/* ./ &&
+git add . &&
+git commit -m "update" &&
+git push &&
 git checkout -
